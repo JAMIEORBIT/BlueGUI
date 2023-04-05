2b12fe1359fc9c6121ded1698a5ae3cfad13be72
JAMIEORBIT <119012537+JAMIEORBIT@users.noreply.github.com>
2023-04-05 18:14:42 +0100

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -142,16 +142,16 @@ function CFAHub:CreateWindow(title, gameName, intro)
 
     local themes = {
         SchemaColor = Color3.fromRGB(79, 195, 247),
-        TextColor = Color3.fromRGB(48, 25, 52),
-        Header = Color3.fromRGB(22, 22, 22),
+        TextColor = Color3.fromRGB(157, 0, 255),
+        Header = Color3.fromRGB(157, 0, 255),
         Container = Color3.fromRGB(34, 34, 34),
         Background = Color3.fromRGB(22, 22, 22),
         Slider = Color3.fromRGB(15, 15, 15),
         Drop = Color3.fromRGB(28, 28, 28),
-        ScrollBar = Color3.fromRGB(48, 25, 52),
+        ScrollBar = Color3.fromRGB(157, 0, 255),
         NotiBackground = Color3.fromRGB(0, 0, 0),
-        Glow = Color3.fromRGB(48, 25, 52),
-        Logo = "rbxassetid://11663634808"
+        Glow = Color3.fromRGB(157, 0, 255),
+        Logo = "rbxassetid://11863446763"
     }
 
     table.insert(CFAHub, title)
